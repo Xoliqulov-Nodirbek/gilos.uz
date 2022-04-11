07ba7565c0a61b1b2b27036158ee2f3e4ad63e6d
Xoliqulov-Nodirbek <xoliqulov.nodirbek2003@gmail.com>
2022-04-11 22:07:00 +0500

    added new change in js file

--- js/main.js
+++ js/main.js
@@ -1,17 +1,94 @@
 const createElement = function(elName, className) {
-    const createdElement = document.createElement(elName);
-    createdElement.className = className;
-    return createdElement
+  const createdElement = document.createElement(elName);
+  createdElement.className = className;
+  return createdElement
 }
 
 const addZero = function(number) {
-    return number < 10 ? "0" + number : number;
+  return number < 10 ? "0" + number : number;
 }
 
 const showDate = function(dateString) {
-    const date = new Date(dateString);
-    
-    return `${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.${date.getFullYear()} ${addZero(date.getHours())}:${addZero(date.getMinutes())}`
+  const date = new Date(dateString);
+  
+  return `${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.${date.getFullYear()} ${addZero(date.getHours())}:${addZero(date.getMinutes())}`
+}
+
+const renderProduct = function(product) {
+  const elProduct = createElement("li", "col-4");
+  
+  const elProductDiv = createElement("div", "card");      // ----------------------- >
+  
+  const elProductImg = createElement("img", "card-img-top");
+  elProductImg.src = product.img;
+  elProductDiv.append(elProductImg);
+  
+  const elCardBody = createElement("div", "card-body");     // -------------------------- >
+  
+  // ------- Title --------
+  const elCardBodyTitle = createElement("h3", "card-title");
+  elCardBodyTitle.textContent = product.title;
+  elCardBody.append(elCardBodyTitle);
+  
+  // -------- New Price --------
+  const elCardBodyPrice = createElement("p", "card-text fw-bold");
+  const elCardBodyMark = createElement("mark");
+  elCardBodyMark.textContent = product.price;
+  elCardBodyPrice.append(elCardBodyMark);
+  elCardBody.append(elCardBodyPrice);
+  
+  // -------- Current Price --------
+  const elCardBodyCurrentPrice = createElement("p", "card-text");
+  const elCardBodyCurrentDel = createElement("s");
+  elCardBodyCurrentDel.textContent = product.price;
+  elCardBodyCurrentPrice.append(elCardBodyCurrentDel);
+  elCardBody.append(elCardBodyCurrentPrice);
+  
+  // ------- Badge -------
+  const elCardBodyBadge = createElement("p", "badge bg-success");
+  elCardBodyBadge.textContent = product.model;
+  elCardBody.append(elCardBodyBadge);
+  
+  // ------- Data -------
+  const elCardBodyDate = createElement("p", "card-text");
+  elCardBodyDate.textContent = showDate( product.addedDate);
+  elCardBody.append(elCardBodyDate);
+  
+  // -------- Benefits -------
+  const elCardBodyBenefits = createElement("ul", "d-flex flex-wrap list-unstyled");
+  for (let j = 0; j < product.benefits.length; j++){
+    const elCardBodyBenefitsItem = createElement("li", "badge bg-primary me-1 mb-1");
+    elCardBodyBenefitsItem.textContent = product.benefits[j];
+    elCardBodyBenefits.append(elCardBodyBenefitsItem);
+  }
+  elCardBody.append(elCardBodyBenefits);
+  
+  
+  // -------- Btn -------
+  const elCardBodyBtnWrapper = createElement("div", "position-absolute top-0 end-0 d-flex");
+  
+  // ------- Edit --------
+  const elCardBodyBtnEdit = createElement("button", "btn rounded-0 btn-secondary");
+  const elCardBodyBtnEditI = createElement("i", "fa-solid fa-pen");
+  elCardBodyBtnEdit.append(elCardBodyBtnEditI);
+  elCardBodyBtnWrapper.append(elCardBodyBtnEdit);
+  
+  
+  
+  // ------- Delete --------
+  const elCardBodyBtnDelete = createElement("button", "btn rounded-0 btn-danger");
+  const elCardBodyBtnDeleteI = createElement("i", "fa-solid fa-trash");
+  elCardBodyBtnDelete.append(elCardBodyBtnDeleteI);
+  elCardBodyBtnWrapper.append(elCardBodyBtnDelete);v 
+  
+  
+  
+  // --------- Appends ---------
+  elCardBody.append(elCardBodyBtnWrapper);
+  elProductDiv.append(elCardBody);
+  elProduct.append(elProductDiv);
+  
+  return elProduct;
 }
 
 // ------------ Wrapper ------------
@@ -20,78 +97,9 @@ const elProductsWrapper = document.querySelector(".products-wrapper");
 
 
 for (let i = 0; i < products.length; i++) {
-    const currentProducts = products[i];
-    
-    const elProduct = createElement("li", "col-4");
-    
-    const elProductDiv = createElement("div", "card");      // ----------------------- >
-    
-    const elProductImg = createElement("img", "card-img-top");
-    elProductImg.src = currentProducts.img;
-    elProductDiv.append(elProductImg);
-    
-    const elCardBody = createElement("div", "card-body");     // -------------------------- >
-    
-    // ------- Title --------
-    const elCardBodyTitle = createElement("h3", "card-title");
-    elCardBodyTitle.textContent = currentProducts.title;
-    elCardBody.append(elCardBodyTitle);
-    
-    // -------- New Price --------
-    const elCardBodyPrice = createElement("p", "card-text fw-bold");
-    const elCardBodyMark = createElement("mark");
-    elCardBodyMark.textContent = currentProducts.price;
-    elCardBodyPrice.append(elCardBodyMark);
-    elCardBody.append(elCardBodyPrice);
-    
-    // -------- Current Price --------
-    const elCardBodyCurrentPrice = createElement("p", "card-text");
-    const elCardBodyCurrentDel = createElement("s");
-    elCardBodyCurrentDel.textContent = currentProducts.price;
-    elCardBodyCurrentPrice.append(elCardBodyCurrentDel);
-    elCardBody.append(elCardBodyCurrentPrice);
-    
-    // ------- Badge -------
-    const elCardBodyBadge = createElement("p", "badge bg-success");
-    elCardBodyBadge.textContent = currentProducts.model;
-    elCardBody.append(elCardBodyBadge);
-    
-    // ------- Data -------
-    const elCardBodyDate = createElement("p", "card-text");
-    elCardBodyDate.textContent = showDate( currentProducts.addedDate);
-    elCardBody.append(elCardBodyDate);
-    
-    // -------- Benefits -------
-    const elCardBodyBenefits = createElement("ul", "d-flex flex-wrap list-unstyled");
-    for (let j = 0; j < products[i].benefits.length; j++){
-        const elCardBodyBenefitsItem = createElement("li", "badge bg-primary me-1 mb-1");
-        elCardBodyBenefitsItem.textContent = products[i].benefits[j];
-        elCardBodyBenefits.append(elCardBodyBenefitsItem);
-    }
-    elCardBody.append(elCardBodyBenefits);
-
-    
-    // -------- Btn -------
-    const elCardBodyBtnWrapper = createElement("div", "position-absolute top-0 end-0 d-flex");
-    
-    // ------- Edit --------
-    const elCardBodyBtnSec = createElement("button", "btn rounded-0 btn-secondary");
-    const elCardBodyBtnSecI = createElement("i", "fa-solid fa-pen");
-    elCardBodyBtnSec.append(elCardBodyBtnSecI);
-    elCardBodyBtnWrapper.append(elCardBodyBtnSec);
-    
-    // ------- Delete --------
-    const elCardBodyBtndan = createElement("button", "btn rounded-0 btn-danger");
-    const elCardBodyBtndanI = createElement("i", "fa-solid fa-trash");
-    elCardBodyBtndan.append(elCardBodyBtndanI);
-    elCardBodyBtnWrapper.append(elCardBodyBtndan);
-    
-    
-    // --------- Appends ---------
-    elCardBody.append(elCardBodyBtnWrapper);
-    elProductDiv.append(elCardBody);
-    elProduct.append(elProductDiv);
-    elProductsWrapper.append(elProduct);
+  const elProduct = renderProduct(products[i]);
+  
+  elProductsWrapper.append(elProduct);
 }
 
 
@@ -106,27 +114,32 @@ elForm.addEventListener("submit", function(evt) {
   evt.preventDefault();
   
   const elements = evt.target.elements;
-
+  
   const productTitleInput = elements["product-title"];
   const priceInput = elements["price"];
   const manufacturerSelect = elements["product-manufacturer"];
-
+  
   const productTitleInputValue = productTitleInput.value;
   const priceInputValue = priceInput.value;
   const manufacturerSelectValue = manufacturerSelect.value;
-
+  
   if (productTitleInputValue.trim() && priceInputValue.trim() && manufacturerSelectValue.trim()) {
     const newCard = {
       id: Math.floor(Math.random() * 1000),
       title: productTitleInputValue,
       img: "https://picsum.photos/300/200",
       price: priceInputValue,
+      benefits: [],
+      model: select.value,
       addedDate: new Date("2021-10-12").toISOString(),
     }
-
+    
     products.push(newCard);
     elForm.reset();
     addProductModal.hide();
+    
+    const product = renderProduct(newCard);
+    elProductsWrapper.append(product);
   }
 })
 
@@ -140,9 +153,27 @@ for (let k = 0; k < manufacturers.length; k++) {
   select.append(option);
 }
 
+// const filterForm = document.querySelector(".form-filter");
 
+// filterForm.addEventListener("submit", function(evt) {
+//   evt.preventDefault();
 
+//   const elements = evt.target.elements;
 
+//   const fromValue = elements.from.value; 
+//   const toValue = elements.to.value;
+
+//   const filteredProduct = products
+//   .filter(function(fromProduct) {
+//     return fromProduct.price >= fromValue;
+//   })
+//   .filter(function(toProduct) {
+//     return toProduct.price <= toValue;
+//   }).filter(function(product) {
+
+//   })
+//   console.log(filteredProduct);
+// });
 
 
 
